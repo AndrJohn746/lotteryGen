e6375047a62f2875642c16eefbc2334b5286f657
Drew Johnson <andrjohn746@ed.amdsb.ca>
2020-05-10 16:47:28 -0400

    Add project files.

---
+++ lotteryGen/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace lotteryGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.infoText = new System.Windows.Forms.Label();
+            this.genButt = new System.Windows.Forms.Button();
+            this.numText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.Color.Wheat;
+            this.title.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(12, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(410, 54);
+            this.title.TabIndex = 0;
+            this.title.Text = "Lottery Number Generator";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoText
+            // 
+            this.infoText.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoText.Location = new System.Drawing.Point(12, 63);
+            this.infoText.Name = "infoText";
+            this.infoText.Size = new System.Drawing.Size(407, 63);
+            this.infoText.TabIndex = 1;
+            this.infoText.Text = "Press the button below to generate your quick-pick Lotto 6/49 numbers!";
+            this.infoText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // genButt
+            // 
+            this.genButt.AutoSize = true;
+            this.genButt.BackColor = System.Drawing.Color.Khaki;
+            this.genButt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.genButt.Location = new System.Drawing.Point(178, 138);
+            this.genButt.Name = "genButt";
+            this.genButt.Size = new System.Drawing.Size(82, 29);
+            this.genButt.TabIndex = 2;
+            this.genButt.Text = "Generate";
+            this.genButt.UseVisualStyleBackColor = false;
+            this.genButt.Click += new System.EventHandler(this.genButt_Click);
+            // 
+            // numText
+            // 
+            this.numText.BackColor = System.Drawing.Color.FloralWhite;
+            this.numText.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numText.Location = new System.Drawing.Point(12, 181);
+            this.numText.Name = "numText";
+            this.numText.Size = new System.Drawing.Size(410, 65);
+            this.numText.TabIndex = 3;
+            this.numText.Text = "Your winning numbers are just one button press away!";
+            this.numText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.numText);
+            this.Controls.Add(this.genButt);
+            this.Controls.Add(this.infoText);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.Text = "Not_A_Scam.exe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label infoText;
+        private System.Windows.Forms.Button genButt;
+        private System.Windows.Forms.Label numText;
+    }
+}
+
---
+++ lotteryGen/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lotteryGen
+{
+    public partial class Form1 : Form
+    {
+
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+        int num4 = 0;
+        int num5 = 0;
+        int num6 = 0;
+        readonly int BASEVAL = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void genButt_Click(object sender, EventArgs e)
+        {
+            num1 = BASEVAL;
+            num2 = BASEVAL;
+            num3 = BASEVAL;
+            num4 = BASEVAL;
+            num5 = BASEVAL;
+            num6 = BASEVAL;
+
+            Random randGen = new Random();
+            num1 = randGen.Next(1, 50);
+
+            while (num2 == BASEVAL || num2 == num1)
+            {
+                num2 = randGen.Next(1, 50);
+            }
+            while (num3 == BASEVAL || num3 == num2 || num3 == num1)
+            {
+                num3 = randGen.Next(1, 50);
+            }
+            while (num4 == BASEVAL || num4 == num3 || num4 == num2 || num4 == num1)
+            {
+                num4 = randGen.Next(1, 50);
+            }
+            while (num5 == BASEVAL || num5 == num4 || num5 == num3 || num5 == num2 || num5 == num1)
+            {
+                num5 = randGen.Next(1, 50);
+            }
+            while (num6 == BASEVAL || num6 == num5 || num6 == num4 || num6 == num3 || num6 == num2 || num6 == num1)
+            {
+                num6 = randGen.Next(1, 50);
+            }
+
+            numText.Text = "Your winning numbers are: ";
+            numText.Text += "\n" + num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " + num5 + ", " + num6;
+
+        }
+    }
+}
